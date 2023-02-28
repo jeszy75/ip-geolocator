87be7dc0adf186e15374b554454f3fc16fb3b536
Péter Jeszenszky <jeszenszky.peter@inf.unideb.hu>
2023-02-28 10:43:21 +0100

    Initial import

---
+++ src/main/java/geolocator/GeoLocator.java
@@ -0,0 +1,31 @@
+package geolocator;
+
+import feign.Feign;
+import feign.Logger;
+import feign.Param;
+import feign.RequestLine;
+import feign.gson.GsonDecoder;
+
+import com.google.gson.GsonBuilder;
+import com.google.gson.FieldNamingPolicy;
+import feign.slf4j.Slf4jLogger;
+
+public interface GeoLocator {
+
+    @RequestLine("GET")
+    GeoLocation getGeoLocation();
+
+    @RequestLine("GET /{ipOrHostName}")
+    GeoLocation getGeoLocation(@Param("ipOrHostName") String ipOrHostName);
+
+    static GeoLocator newInstance() {
+        return Feign.builder()
+                .decoder(new GsonDecoder(new GsonBuilder()
+                        .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                        .create()))
+                .logger(new Slf4jLogger())
+                .logLevel(Logger.Level.FULL)
+                .target(GeoLocator.class, "https://reallyfreegeoip.org/json/");
+    }
+
+}
